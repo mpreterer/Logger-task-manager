0656b7d74483b321a74de2ef20d23f28cffe4477
Ivanov Alexey <ivanovdev@disroot.org>
2021-10-07 12:01:46 +0300

    added materialui and sampel button

---
+++ components/sample-mui.tsx
@@ -0,0 +1,11 @@
+import Button from '@material-ui/core/Button';
+
+function Buttonui() {
+  return (
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+  );
+}
+
+export default Buttonui;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import Sample from '../components/sample';
+import Buttonui from '../components/sample-mui';
 
 function HomePage() {
-  return <Sample name="Hello, world!" />;
+  return (
+    <>
+      <Buttonui />
+      <Sample name="Hello, world!" />
+    </>
+  );
 }
 
 export default HomePage;
