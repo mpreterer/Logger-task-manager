6d6c84570687d421dc4aeef61cd21a6b8d6fac71
Ivanov Alexey <ivanovdev@disroot.org>
2021-10-08 23:20:42 +0300

    check route in autorisetion page

--- components/loginButton.tsx
+++ components/loginButton.tsx
@@ -1,10 +1,13 @@
 import { observer } from 'mobx-react';
 import { Button } from '@mui/material';
+import Link from 'next/link';
 
 const LoginButton = observer(({ authorization }) => (
-  <Button variant="contained" onClick={() => authorization.login()}>
-    Authorization
-  </Button>
+  <Link href="/">
+    <Button variant="contained" onClick={() => authorization.login()}>
+      Authorization
+    </Button>
+  </Link>
 ));
 
 export default LoginButton;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,23 @@
 import LoginButton from '../components/loginButton';
 import authorization from '../states/authorization';
+import Link from 'next/link';
 
 function HomePage() {
-  return <LoginButton authorization={authorization} />;
+  if (authorization.isLogin) {
+    return (
+      <>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <LoginButton authorization={authorization} />
+    </>
+  );
 }
 
 export default HomePage;
