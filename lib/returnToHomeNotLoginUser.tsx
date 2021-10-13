06257a577952a276bf8ea6ecb3e411d62b5cb88f
Ivanov Alexey <ivanovdev@disroot.org>
2021-10-13 17:35:04 +0300

    fix error when user not login and open first board page

--- lib/returnToHomeNotLoginUser.tsx
+++ lib/returnToHomeNotLoginUser.tsx
@@ -1,17 +1,21 @@
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 import authorization from '../states/authorization';
 
-function returnToHomeNotLoginUser() {
-  if (authorization.isLogin) {
-    return <div>loading...</div>;
-  }
+const useUser = () => ({ isLogin: authorization.isLogin });
 
+function returnToHomeNotLoginUser() {
+  const { isLogin } = useUser();
   const router = useRouter();
 
-  (() => {
-    router.push('/');
-  })();
+  useEffect(() => {
+    if (!isLogin) {
+      router.push('/');
+    }
+  }, [isLogin]);
+
+  return <p>Redirecting...</p>;
 }
 
 export default returnToHomeNotLoginUser;
