16bc6fbdf69eacf7de913633674c2e34579b4cc1
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-10-17 22:10:01 +0300

    add page for trello return url

---
+++ pages/trello/auth.tsx
@@ -0,0 +1,26 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+import AuthService from "../../services/trello/AuthService";
+
+const Auth = () => {
+  const router = useRouter();
+
+  // Browser side
+  useEffect(() => {
+    const tokenHashName = `${process.env.NEXT_PUBLIC_TOKEN_HASH_NAME}`;
+    const urlHash = router.asPath.replace(router.pathname, '');
+    const token = urlHash.replace(tokenHashName, '').replace(/[\s#=]/g, '');
+    const isToken = token !== '';
+
+    if(isToken) {
+      AuthService.saveToken(token); // save the token to localStorage
+    }
+    
+    window.location.href = '/'; // redirect to the home page
+  })
+
+  return (<>Загрузка...</>);
+}
+
+export default Auth;
