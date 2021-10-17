674b8768037c74fbb26abf602e24d7fb6ab2c123
Ivanov Alexey <ivanovdev@disroot.org>
2021-10-17 10:01:45 +0300

    added imeges to header button, logo to header and change style header button in active page

--- components/header.tsx
+++ components/header.tsx
@@ -1,24 +1,82 @@
-import { AppBar, Button, Box, Toolbar } from '@mui/material';
+import { AppBar, Button, Box, Toolbar, Typography } from '@mui/material';
 import Link from 'next/link';
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 import UserAvatar from './avatar';
+import HomeIcon from '../public/images/home.svg';
+import BoardsIcon from '../public/images/boards.svg';
+import LogoIcon from '../public/images/logo.svg';
 
 const Header = () => {
+  const router = useRouter();
+
   return (
-    <AppBar position="static" sx={{ height: '49px' }}>
-      <Toolbar variant="dense" sx={{ gap: '4px' }}>
-        <Link href="/">
-          <Button variant="contained">
-            <HomeOutlinedIcon />
-          </Button>
-        </Link>
-        <Link href="/boards">
-          <Button variant="contained">Boards</Button>
-        </Link>
-        <Link href="/creating-board">
-          <Button variant="contained">Creating board</Button>
-        </Link>
+    <AppBar position="static" sx={{ height: '49px', backgroundColor: '#026AA7' }}>
+      <Toolbar variant="dense" sx={{ display: 'grid', gridTemplateColumns: '1fr auto 1fr' }}>
+        <Box sx={{ display: 'flex', gap: '4px' }}>
+          <Link href="/">
+            <Button
+              variant="contained"
+              sx={{
+                width: '41px',
+                height: '41px',
+                boxSizing: 'border-box',
+                padding: '0',
+                backgroundColor: router.asPath === '/' ? '#284968' : '#4E97C2',
+              }}
+            >
+              <Image src={HomeIcon} width={17} height={19} alt="" />
+            </Button>
+          </Link>
+          <Link href="/boards">
+            <Button
+              variant="contained"
+              sx={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                width: '160px',
+                backgroundColor: router.asPath === '/boards' ? '#284968' : '#4E97C2',
+                textTransform: 'capitalize',
+                fontSize: '20px',
+                height: '41px',
+                letterSpacing: 'normal',
+              }}
+            >
+              <Image src={BoardsIcon} alt="" /> my boards
+            </Button>
+          </Link>
+          <Link href="/creating-board">
+            <Button
+              variant="contained"
+              sx={{
+                width: '142px',
+                backgroundColor: router.asPath === '/creating-board' ? '#284968' : '#4E97C2',
+                textTransform: 'capitalize',
+                fontSize: '20px',
+                padding: '0',
+                height: '41px',
+                letterSpacing: 'normal',
+              }}
+            >
+              create board
+            </Button>
+          </Link>
+        </Box>
+        <Box sx={{ display: 'flex' }}>
+          <Image src={LogoIcon} alt="" />
+          <Typography
+            component="h1"
+            variant="h4"
+            color="#80B4D3"
+            fontStyle="italic"
+            fontWeight={500}
+            fontSize={38}
+            line-height={45}
+          >
+            Crello
+          </Typography>
+        </Box>
         <Box sx={{ marginLeft: 'auto' }}>
           <UserAvatar size="mini" />
         </Box>
