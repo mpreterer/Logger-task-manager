b5076c86ac10a659fcc2a26450e98841e3d68989
Ivanov Alexey <ivanovdev@disroot.org>
2021-10-20 21:34:34 +0300

    todo-in-progress

--- pages/board.tsx
+++ pages/board.tsx
@@ -1,34 +1,197 @@
-import { Box } from '@mui/system';
-import PreviewCard from '../components/previewCard';
+import { Box } from '@mui/material';
+
+import ColumnCard from '../components/columnCard';
 
 function Board() {
+  const data = [
+    {
+      title: 'test',
+      previewCards: [
+        {
+          text: 'Define more tags in components',
+          date: 'Mar 3, 2020',
+          difficulty: 'high',
+          members: [
+            { alt: 'alt', url: '' },
+            { alt: 'alt', url: '' },
+            { alt: 'alt', url: '' },
+          ],
+        },
+        {
+          text: 'Define more tags in components',
+          date: 'Mar 3, 2020',
+          difficulty: 'high',
+          members: [
+            { alt: 'alt', url: '' },
+            { alt: 'alt', url: '' },
+            { alt: 'alt', url: '' },
+          ],
+        },
+        {
+          text: 'Define more tags in components',
+          date: 'Mar 3, 2020',
+          difficulty: 'high',
+          members: [
+            { alt: 'alt', url: '' },
+            { alt: 'alt', url: '' },
+            { alt: 'alt', url: '' },
+          ],
+        },
+      ],
+    },
+    {
+      previewCards: [
+        {
+          text: 'Define more tags in components',
+          date: 'Mar 3, 2020',
+          difficulty: 'high',
+          members: [
+            { alt: 'alt', url: '' },
+            { alt: 'alt', url: '' },
+            { alt: 'alt', url: '' },
+          ],
+        },
+        {
+          text: 'Define more tags in components',
+          date: 'Mar 3, 2020',
+          difficulty: 'high',
+          members: [
+            { alt: 'alt', url: '' },
+            { alt: 'alt', url: '' },
+            { alt: 'alt', url: '' },
+          ],
+        },
+        {
+          text: 'Define more tags in components',
+          date: 'Mar 3, 2020',
+          difficulty: 'high',
+          members: [
+            { alt: 'alt', url: '' },
+            { alt: 'alt', url: '' },
+            { alt: 'alt', url: '' },
+          ],
+        },
+      ],
+    },
+    {
+      previewCards: [
+        {
+          text: 'Define more tags in components',
+          date: 'Mar 3, 2020',
+          difficulty: 'low',
+        },
+        {
+          text: 'Define more tags in components',
+          date: 'Mar 3, 2020',
+          difficulty: 'medium',
+          members: [
+            { alt: 'alt', url: '' },
+            { alt: 'alt', url: '' },
+          ],
+        },
+        {
+          text: 'Define more tags in components',
+          date: 'Mar 3, 2020',
+          difficulty: 'high',
+          members: [
+            { alt: 'alt', url: '' },
+            { alt: 'alt', url: '' },
+            { alt: 'alt', url: '' },
+          ],
+        },
+      ],
+    },
+    {
+      previewCards: [
+        {
+          text: 'Define more tags in components',
+          date: 'Mar 3, 2020',
+          difficulty: 'high',
+          members: [
+            { alt: 'alt', url: '' },
+            { alt: 'alt', url: '' },
+            { alt: 'alt', url: '' },
+          ],
+        },
+        {
+          text: 'Define more tags in components',
+          date: 'Mar 3, 2020',
+          difficulty: 'high',
+          members: [
+            { alt: 'alt', url: '' },
+            { alt: 'alt', url: '' },
+            { alt: 'alt', url: '' },
+          ],
+        },
+        {
+          text: 'Define more tags in components',
+          date: 'Mar 3, 2020',
+          difficulty: 'high',
+          members: [
+            { alt: 'alt', url: '' },
+            { alt: 'alt', url: '' },
+            { alt: 'alt', url: '' },
+          ],
+        },
+      ],
+    },
+    {
+      previewCards: [
+        {
+          text: 'Define more tags in components',
+          date: 'Mar 3, 2020',
+          difficulty: 'high',
+          members: [
+            { alt: 'alt', url: '' },
+            { alt: 'alt', url: '' },
+            { alt: 'alt', url: '' },
+          ],
+        },
+        {
+          text: 'Define more tags in components',
+          date: 'Mar 3, 2020',
+          difficulty: 'high',
+          members: [
+            { alt: 'alt', url: '' },
+            { alt: 'alt', url: '' },
+            { alt: 'alt', url: '' },
+          ],
+        },
+        {
+          text: 'Define more tags in components',
+          date: 'Mar 3, 2020',
+          difficulty: 'high',
+          members: [
+            { alt: 'alt', url: '' },
+            { alt: 'alt', url: '' },
+            { alt: 'alt', url: '' },
+          ],
+        },
+      ],
+    },
+  ];
+
   return (
     <Box
       sx={{
-        width: '298px',
-        padding: '16px',
-        backgroundColor: 'white',
-        display: 'grid',
-        gap: '20px',
+        display: 'flex',
+        gap: '41px',
+        alignItems: 'flex-start',
+        padding: '42px 66px',
+        overflowX: 'auto',
+        height: 'calc(100vh - 56px)',
+        '&::-webkit-scrollbar': {
+          width: '15px',
+        },
+        '&::-webkit-scrollbar-thumb': {
+          backgroundColor: '#A6AAAC',
+          borderRadius: '40px',
+        },
       }}
     >
-      <PreviewCard text="Define more tags in components" date="Mar 3, 2020" difficulty="low" />
-      <PreviewCard
-        text="Define more tags in components"
-        date="Mar 3, 2020"
-        difficulty="medium"
-        members={[{ alt: 'alt', url: '' }]}
-      />
-      <PreviewCard
-        text="Define more tags in components"
-        date="Mar 3, 2020"
-        difficulty="high"
-        members={[
-          { alt: 'alt', url: '' },
-          { alt: 'alt', url: '' },
-          { alt: 'alt', url: '' },
-        ]}
-      />
+      {data.map((item) => {
+        return <ColumnCard previewCards={item.previewCards} title={item.title} />;
+      })}
     </Box>
   );
 }
