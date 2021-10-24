bca020666d03e72c98dd96708d49c85c235b89b2
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-10-24 13:39:39 +0300

    removed unnecessary logic

--- services/trello/ApiCall.ts
+++ services/trello/ApiCall.ts
@@ -1,51 +1,25 @@
+import axios from "axios";
+
 class ApiCall {
   
   private trelloDomain: string;
-  private key: string;
 
   constructor() {
-    this.trelloDomain = 'https://api.trello.com/1/';
-    this.key = `${process.env.NEXT_PUBLIC_TRELLO_KEY}`;
+    this.trelloDomain = 'https://api.trello.com/1';
   }
 
-  public async perform (url: string, data?: any, config?: any) {
-    const token = this.getToken();
-    const request = await fetch(`${this.trelloDomain}/${url}/?key=${this.key}&token=${token}`, {
+  public async instance (url: string, data?: any, config?: any) {
+    const token = localStorage.getItem(`trello-auth-token`) || '';
+    
+    return await axios(`${this.trelloDomain}/${url}`, {
       ...config,
       body: JSON.stringify(data),
       headers: {
-        'Accept': 'application/json'
+        'Accept': 'application/json',
+        'Authorization':
+          `OAuth oauth_consumer_key=\"${process.env.NEXT_PUBLIC_TRELLO_KEY}\", oauth_token=\"${token}\"`
       },
     });
-
-    return await request.json();
-  }
-
-  public async get (path: string) {
-    return await this.perform(`${path}`);
-  }
-
-  public async post (path: string, data: any) {
-    return await this.perform(`${path}`, data, {
-      method: 'POST'
-    });
-  }
-
-  public async put (path: string, data: any) {
-    return await this.perform(`${path}`, data, {
-      method: 'PUT'
-    });
-  }
-
-  public async delete (path: string) {
-    return await this.perform(`${path}`, {
-      method: 'DELETE'
-    });
-  }
-
-  private getToken(): string {
-    const token: string = localStorage.getItem(`${process.env.NEXT_PUBLIC_TOKEN_STORAGE_NAME}`) || '';
-    return token;
   }
 }
 
