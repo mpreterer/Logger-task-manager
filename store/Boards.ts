a6a453f70d65749834e9b3b9198161a2906864a0
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-10-24 13:38:34 +0300

    refactoring Boards store

--- store/Boards.ts
+++ store/Boards.ts
@@ -1,22 +1,80 @@
 import { types } from 'mobx-state-tree';
+
 import ApiCall from '../services/trello/ApiCall';
 
-const Board = types.model('Board', {
-  id: types.string,
+const Member = types.model('Member', {
+  id: types.identifier,
+  fullName: types.string,
+  avatarUrl: types.string,
+});
+
+const CardComment = types.model('CardComment', {
+  id: types.identifier,
+  text: types.string,
+  member: Member,
+});
+
+const Card = types.model('Card', {
+  id: types.identifier,
   name: types.string,
   desc: types.string,
+  dateLastActivity: types.string,
+  labels: types.optional(types.array(types.string), []),
+  members: types.array(Member),
+  // comments: types.optional(types.array(CardComment), []),
+});
+
+const List = types.model('List', {
+  id: types.identifier,
+  name: types.string,
+  cards: types.optional(types.array(Card), []),
+});
+
+const Board = types.model('Board', {
+  id: types.identifier,
+  name: types.string,
+  desc: types.string,
+  members: types.optional(types.array(Member), []),
+  lists: types.optional(types.array(List), []),
 });
 
 const BoardsStore = types.model('BoardsStore', {
-  boards: types.array(Board),
+  boards: types.optional(types.array(Board), []),
 }).actions(self => {
   return {
-    addBoards: (boards) => {
-      self.boards = boards;
+    addBoard (board) {
+      self.boards.push(board);
     },
     afterCreate() {
-      ApiCall.get('member/me/boards').then(res => {
-        self.addBoards(res);
+      ApiCall.instance('member/me/boards').then(response => {
+        response.data.map(board => {
+          ApiCall.instance(`boards/${board.id}?lists=all&cards=all&members=all`).then(res => {
+            const confirmedBoard = {
+              id: res.data.id,
+              name: res.data.name,
+              desc: res.data.desc,
+              members: res.data.members,
+              lists: res.data.lists.map(list => {
+                return {
+                  id: list.id,
+                  name: list.name,
+                  cards: res.data.cards
+                    .filter(card => card.idList === list.id)
+                      .map(card => {
+                        card.members = res.data.members
+                        card.labels = card.labels.map(label => {
+                          return label.color;
+                        });
+  
+                        return card;
+                  }),
+                }
+              }),
+            };
+
+            self.addBoard(confirmedBoard)
+          })
+        }) 
       })
     }
   }
