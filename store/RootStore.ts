e0ba283372b88a7f889e9953d71cf65f42c99bfd
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-10-31 19:35:46 +0300

    delete old store

--- store/RootStore.ts
+++ store/RootStore.ts
@@ -1,10 +1,12 @@
-import { types } from 'mobx-state-tree';
-import BoardsStore from './Boards';
-import UserStore from './User';
+import UserStore from './UserStore';
+import BoardsStore from './BoardsStore';
+import { createContext } from 'react';
 
-const RootStore = types.model('RootStore', {
-  user: types.optional(UserStore, {}),
-  boards: types.optional(BoardsStore, {}),
-});
+const RootStore = {
+  user: UserStore,
+  boards: BoardsStore,
+};
+
+export const StoreContext = createContext(RootStore);
 
 export default RootStore;
