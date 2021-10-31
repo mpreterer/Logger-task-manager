a0acfc76e3eaa450a462a5f950356137c59a0895
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-10-31 17:21:56 +0300

    fix return_url

--- components/loginButton.tsx
+++ components/loginButton.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import { Button } from '@mui/material';
 
 const LoginButton = () => {
-  const trelloLogin = `https://trello.com/1/authorize?response_type=token&key=${process.env.NEXT_PUBLIC_TRELLO_KEY}&return_url=http%3A%2F%2F127.0.0.1:3000&callback_method=fragment&scope=read%2Cwrite%2Caccount&expiration=never&name=Crello`;
+  const trelloLogin = `https://trello.com/1/authorize?response_type=token&key=${process.env.NEXT_PUBLIC_TRELLO_KEY}&return_url=http://localhost:3000/trello/auth&callback_method=fragment&scope=read,write&expiration=never&name=Crello`;
 
   return (
     <Link href={trelloLogin}>
