f7609947ab8b1859078892d43da1e9c46b7a3a1d
Timofey Syromyatnikov <warfacetopic@mail.ru>
2021-11-06 20:14:18 +0300

    refactoring page styles

--- pages/creating-board.tsx
+++ pages/creating-board.tsx
@@ -2,16 +2,23 @@ import { observer } from 'mobx-react';
 import { Container } from '@mui/material';
 import { Box } from '@mui/system';
 
-import CreatingBoardCard from '../components/creatingBoardCard';
+import FormCreateBoard from '../components/formCreateBoard/formCreateBoard';
 
 const CreatingBoard = observer(() => {
   return (
     <Container>
-      <Box sx={{ mt: '14%', width: '420px' }}>
-        <CreatingBoardCard />
+      <Box
+        sx={{
+          height: 'calc(100vh - 49px)',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <FormCreateBoard />
       </Box>
     </Container>
   );
-})
+});
 
 export default CreatingBoard;
