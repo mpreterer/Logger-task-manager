74f00ad8018c313838039fd1b4df00eea28516bb
mpreter <dshabelyan@mail.ru>
2023-04-27 15:38:03 +0300

    Added requests for label, card, board (remove, update, get, post info)

--- store/BoardsStore.ts
+++ store/BoardsStore.ts
@@ -1,6 +1,6 @@
 import { makeAutoObservable, runInAction } from 'mobx';
 
-import instance from '../services/trelloInstance';
+import instance, { getToken } from '../services/trelloInstance';
 import IActiveBoard from '../utils/interfaces/IActiveBoard';
 import IActiveCard from '../utils/interfaces/IActiveCard';
 import IBoard from '../utils/interfaces/IBoard';
@@ -45,7 +45,7 @@ class BoardsStore {
       .post('list', list)
       .then((res) => {
         runInAction(() => {
-          this.activeBoard?.lists?.push(res.data);
+          this.activeBoard?.lists?.unshift(res.data);
           console.log('POST List created successfully!');
         });
       })
@@ -195,6 +195,86 @@ class BoardsStore {
       .catch((e) => {
         console.log(e);
       });
+  }
+
+  public async deleteBoardLabel(labelId: string) {
+    await instance.delete(`labels/${labelId}`).catch((e) => {
+      console.log(e);
+    });
+  }
+
+  public async haveLabels(cardId: any) {
+    const label = await instance.get(`cards/${cardId}/labels`);
+    return label;
+  }
+
+  public async getIdBoard(cardId: any) {
+    const card = await instance.get(`cards/${cardId}/board?idBoard`);
+    const boardId = card.data.id;
+    return boardId;
+  }
+
+  public async createNewLabel(idBoard: any, color: string, name: string) {
+    const label = await instance.post(`labels?name=${name}&color=${color}&idBoard=${idBoard}`);
+    return label.data.id;
+  }
+
+  public async addLabelToCard(cardId: any, labelId: any) {
+    try {
+      await instance.post(`cards/${cardId}/idLabels`, {
+        value: labelId,
+      });
+      console.log(`Label added to card with ID ${cardId}`);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  public async updateCardPosition(cardId: any, listId: any, index: number) {
+    const data = {
+      pos: index,
+    };
+
+    await instance.put(`cards/${cardId}?idList=${listId}`, data);
+  }
+
+  public async addedDescriptionCard(cardId: any, description: any) {
+    instance
+      .put(`cards/${cardId}/desc`, {
+        value: description,
+      })
+      .then(() => {
+        alert('Card description update');
+      })
+      .catch((error) => {
+        console.error('Error creating card description:', error.response.data);
+      });
+  }
+
+  public async getDescription(id: any) {
+    const card = await instance.get(`cards/${id}/desc`);
+    const desc = card.data;
+    return desc;
+  }
+
+  public async getBackgroundColor(boardId: any) {
+    const board = await instance.get(`boards/${boardId}/prefs`);
+    const desc = board.data;
+    return desc;
+  }
+
+  public async removeList(listId: any) {
+    await instance.put(`lists/${listId}`, {
+      closed: true,
+    });
+  }
+
+  public async removeCard(cardId: any) {
+    await instance.delete(`cards/${cardId}`);
+  }
+
+  public async getInfoCard(cardId: any) {
+    await instance.delete(`cards/${cardId}`);
   }
 }
 
