3b38e7fd144e31e7337df090ea7200c7a8358884
mpreter <dshabelyan@mail.ru>
2023-04-27 15:40:49 +0300

    Added for board list background color

--- components/boardsList.tsx
+++ components/boardsList.tsx
@@ -14,6 +14,7 @@ const BoardsList = observer(() => {
 
   const boardCards = boards.boards.map((item: IBoard) => (
     <BoardCard
+      color={item?.prefs.backgroundColor}
       name={item?.name}
       desc={item?.desc}
       id={item?.id}
